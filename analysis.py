d63e5729cc2acce4b431aac62b1d8aa7aeab3cc2
Damon Wen(chang) <mnn.fantasy@gmail.com>
2013-12-12 22:47:47 +0800

    log file analysis improvments
    major changes:
    1. detailed error keywords
    2. check 3 log files in the same round; if any error occurs, return
    error

--- analysis.py
+++ analysis.py
@@ -1,50 +1,53 @@
 import os
 import GLOBAL
 
-keyword_arr = ['error',
-'shit',
-'fuck'
+keyword_arr = ['Called Memory smash',
+'A packet data pointer has size',
+'Call Trace',
+'NMI Watchdog interrupt',
+'Unable to handle kernel',
+'Segmentation fault',
+'bus error',
+'Failed WQE allocate',
+'UBIFS error',
+'ECC'
 ]
 
-# return   inexiste:-1   error/null:0    right:1
-def filecheck(int_val):
-    filename = str(int_val)
-    filename = './log/round' + str(GLOBAL.round) + '/' + filename
-    if os.path.exists(filename) == False:
-        return -1
-    if os.path.getsize(filename) == 0:
-        return 0
-    f = open(filename, 'r')
-    while True:
-        line = f.readline()
-        if len(line) == 0:
-            f.close()
-            return 1
-        for keyword in keyword_arr:
-            if line.find(keyword) != -1:
-                f.close()
-                return 0
+filelist = ['disk',
+'led',
+'nic'
+]
 
+# return  inexiste:-1 error/null:0 right:1
+def filecheck(int_val):
+    #filename = str(int_val)
+    for file in filelist:
+        filename = ''
+        filename = './log/round' + str(GLOBAL.round) + '/' + str(int_val) + '_' + file
+        #print filename
+        if os.path.exists(filename) == False:
+            return -1
+        if os.path.getsize(filename) == 0:
+            return 0
+        f = open(filename, 'r')
+        while True:
+            line = f.readline()
+            if len(line) == 0:
+                f.close()
+                break
+            for keyword in keyword_arr:
+                if line.find(keyword) != -1:
+                    f.close()
+                    return 0
+    return 1
 
 if __name__ == '__main__':
-    ret = filecheck(1)
+    ret = filecheck(19)
     print ret
-    ret = filecheck(2)
-    print ret
-    ret = filecheck(3)
-    print ret
-    ret = filecheck(4)
-    print ret
-    ret = filecheck(5)
-    print ret
-    ret = filecheck(6)
-    print ret
-    ret = filecheck(7)
-    print ret
-    ret = filecheck(8)
-    print ret
-    ret = filecheck(9)
-    print ret
-    ret = filecheck(10)
-    print ret
+    #ret = filecheck(2)
+    #print ret
+    #ret = filecheck(3)
+    #print ret
+    #ret = filecheck(4)
+    #print ret
 

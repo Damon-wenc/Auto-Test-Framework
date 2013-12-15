2704b236f61bb425533cb74b13e720aec7f1c038
Damon Wen(chang) <mnn.fantasy@gmail.com>
2013-12-15 21:45:14 +0800

    Added 'stop' function
    I add the STOP function and I modify some function name for reusing.

--- run_test.py
+++ run_test.py
@@ -49,7 +49,7 @@ def check_log():
         else:
             continue
 
-class enc_start_test(threading.Thread):
+class enc_run_cmd(threading.Thread):
     def __init__(self, name, cmd):
         threading.Thread.__init__(self)
         self.name = name
@@ -58,26 +58,30 @@ class enc_start_test(threading.Thread):
         print self.name
         ssh.run(self.name, self.cmd)
                     
-def cli_start():
+def send_cmd(cmd):
     print GLOBAL.enc_status
     for name,address in GLOBAL.enc_status.items():
         if address == 1:       
-            t = enc_start_test(name, 'start')
+            t = enc_run_cmd(name, cmd)
             t.start()
             t.join()
         else:
             #print 'enc skipped'
             continue
 
-def run():
-    #cli_start()
+def start():
+    if 1 == GLOBAL.round:
+        send_cmd('start')
     get_log()
     check_log()
+    
+def stop():
+    send_cmd('stop')
 
 if __name__ == '__main__':
     #print GLOBAL.enc_status
     #run()
     #print GLOBAL.enc_status
     GLOBAL.enc_status = {19 : 1}
-    get_log()
+    stop()
     #print GLOBAL.enc_status

cab77f8429b40c1258deee7891aeb94cb9ecc9fb
Damon-wenc <mnn.fantasy@gmail.com>
2013-12-17 15:03:21 +0800

    Added some comments

--- GLOBAL.py
+++ GLOBAL.py
@@ -8,15 +8,20 @@ Global variables in multi files
 '''
 
 
-enc_status = {}
-
+#The number of devices you want to run test on
 max_enc = 100
 
+#this dict stores device status
+#currently only three possible values, 0:offline, 1:good, -1:error  
+enc_status = {}
 for i in range (101, max_enc + 101):
     enc_status[i] = 0
 
+#I use the prefix with index to compose a complete ip address, like '192.168.2.1', '192.168.2.2' 
 ipaddr = '192.168.2.'
 
+#
 interval = 60 * 30 #30 minutes
 
+#
 test_round = 1

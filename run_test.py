8d8202aa4658605d5a043f134328c09ab13e7a7d
Damon-wenc <mnn.fantasy@gmail.com>
2013-12-24 17:14:14 +0800

    WTF... I'm a idiot... I was totally wrong about how to use threading now I believe I've corrected them.. er.. embarrassing

--- run_test.py
+++ run_test.py
@@ -25,10 +25,16 @@ class retrieve_log(threading.Thread):
     def run(self):
         urllib.urlretrieve(self.url, "./log/round%d/%s" %(GLOBAL.test_round, self.name))
         
-def get_log(name):
-    t1 = retrieve_log(name, 'heat')
-    t1.start()
-    t1.join()
+def get_log():
+    thread_pool = []
+    for name, address in GLOBAL.enc_status.items():
+        if 1 == address:
+            thread_pool.append(retrieve_log(name, 'heat'))
+    for t in thread_pool:
+        t.start()
+        time.sleep(1)
+    for t in thread_pool:
+        t.join()
 
 class check_log_status(threading.Thread):
     def __init__(self, index):
@@ -41,41 +47,51 @@ class check_log_status(threading.Thread):
         else:
             GLOBAL.enc_status[self.index] = -1
 
-def check_log(name):
-    t = check_log_status(name)
-    t.start()
-    t.join()
-
-class enc_run_cmd(threading.Thread):
-    def __init__(self, name, cmd):
-        threading.Thread.__init__(self)
-        self.name = name
-        self.cmd = cmd
-    def run(self):
-        ssh.run(self.name, self.cmd)
-                    
-def send_cmd(name, cmd):
-    t = enc_run_cmd(name, cmd)
-    t.start()
-    t.join()
-
-def set_log_dir(name):
-    GLOBAL.log_dir[name] = GLOBAL.test_round
-
-def start():
+def check_log():
+    thread_pool = []
     for name, address in GLOBAL.enc_status.items():
         if 1 == address:
-            if 1 == GLOBAL.test_round:
-                send_cmd(name, 'start')
-                time.sleep(15)
-            get_log(name)
-            check_log(name)
-            set_log_dir(name)
+            thread_pool.append(check_log_status(name))
+    for t in thread_pool:
+        t.start()
+        time.sleep(1)
+    for t in thread_pool:
+        t.join()
+                    
+def send_cmd(cmd):
+    thread_pool = []
+    for name, address in GLOBAL.enc_status.items():
+        if 'start' == cmd:
+            if 1 == address:
+                thd = threading.Thread(target = ssh.run, args = (name, cmd))
+                thread_pool.append(thd)
+        else:
+            if 0 != address:
+                thd = threading.Thread(target = ssh.run, args = (name, cmd))
+                thread_pool.append(thd)
+    
+    for t in thread_pool:
+        t.start()
+        time.sleep(1)
+    for t in thread_pool:
+        t.join()
+
+def set_log_dir():
+    for name, address in GLOBAL.enc_status.items():
+        if 1 == address:
+            GLOBAL.log_dir[name] = GLOBAL.test_round
+
+def start():
+    if 1 == GLOBAL.test_round:
+        send_cmd('start')
+        time.sleep(15)
+    set_log_dir()
+    get_log()
+    check_log()
+
     
 def stop():
-    for name, address in GLOBAL.enc_status.items():
-        if 0 != address:
-            send_cmd(name, 'stop')
+    send_cmd('stop')
 
 if __name__ == '__main__':
     GLOBAL.enc_status = {19 : 1}

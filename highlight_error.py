1b376d18c4359ad7863dc3d6c232c38e1b8e1127
Damon-wenc <mnn.fantasy@gmail.com>
2014-03-28 10:18:23 +0800

    auto get new added log files

--- highlight_error.py
+++ highlight_error.py
@@ -8,34 +8,29 @@ Automatically show error info among a various of log files
 '''
 
 import os
-import GLOBAL
 import time
 
 def run():
-    log_round = 0
+    checked_round = 0
     interval = 60# * 3
 
     while True:
-        if log_round > GLOBAL.test_round: #it means a new test loop begin
-            print "*" * 30
-            log_round = 1
-
-        if log_round == GLOBAL.test_round:
-            time.sleep(interval)
-
+        checking_round = 0
         log_path = os.path.join(os.getcwd(), 'log')
         for i in os.listdir(log_path):
             if i.find('round') == 0:
-                log_round += 1
-                tmp_round = 0
-                if tmp_round == log_round:
+                #print checking_round, checked_round
+                checking_round += 1
+                if checking_round <= checked_round: #log folders have already been checked
                     continue
+                #print checking_round, checked_round
                 for j in os.listdir(os.path.join(log_path, i)):
                     print j
-                tmp_round += 1
+                checked_round += 1
+        time.sleep(10)
 
-        break
-        time.sleep(3)
+        #break
+        
 
 
 if __name__ == '__main__':
